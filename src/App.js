05e4b917d19d70afb67dbd190a69f97543ee3ac1
minh1304 <vtm1304@gmail.com>
2022-09-27 15:38:56 +0700

    xong: useState với input radio

--- src/App.js
+++ src/App.js
@@ -2,18 +2,38 @@ import logo from './logo.svg';
 // import './App.css';
 import {useState} from 'react'
 
+const courses = [
+  {
+    id: 1,
+    name: 'HTML/CSS'
+  },
+  {
+    id: 2,
+    name: "JavaScript"
+  },
+  {
+    id: 3,
+    name: "ReactJS"
+  }
+]
+
 function App() {
   const [name,setName] = useState('')
   const [email,setEmail] = useState('')
+  const [checked,setChecked] = useState(1)
+
   const handelSubmit = () => {
     //Call API  
     console.log({
       name, 
-      email
+      email,
+      id: checked
     });
   } 
   return (
     <div style={{padding:32}}>
+
+
       <input 
         value={name}
         onChange={e => setName(e.target.value)}
@@ -23,6 +43,17 @@ function App() {
         value={email}
         onChange={e => setEmail(e.target.value)}
       />
+      {courses.map(course => (
+        <div key={course.id}>
+          <input
+            type="radio" 
+            checked ={checked === course.id}
+            onChange={() => setChecked(course.id)}>
+          </input> 
+          {course.name}
+        </div>
+      ))}
+
       <button onClick={handelSubmit}>Change</button> 
     </div>
   )
