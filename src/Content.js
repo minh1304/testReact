0847b9a072e1df93907235e0a0437334480728c8
minh1304 <vtm1304@gmail.com>
2022-10-03 16:04:40 +0700

    useEffect vs DOM event

--- src/Content.js
+++ src/Content.js
@@ -5,6 +5,7 @@ function Content() {
     const [title, setTitle] =useState('')
     const [posts, setPost] = useState([])
     const [type, setType] = useState('posts')
+    const [showGoToTop,setShowGoToTop] = useState(false)
 
 
     useEffect(()=>{
@@ -14,6 +15,23 @@ function Content() {
             setPost(posts)
         })
     }, [type])
+
+    useEffect( () => {
+        const handleScroll = () => {
+            if(window.scrollY >=200) {
+                setShowGoToTop(true)
+            }
+            else {
+                setShowGoToTop(false)
+            }
+            //setShowGoToTop(window.scrollY >=200)
+        }
+        window.addEventListener('scroll', handleScroll)
+        //Clean function
+        return () => {
+            window.removeEventListener('scroll', handleScroll)
+        }
+    }, [])
     return (
         <div>
             {tabs.map(tab => (
@@ -23,9 +41,7 @@ function Content() {
                         color: '#fff',
                         backgroundColor: '#333'
                     } : {}}
-                    onClick={()=> setType(tab)}
-
-                    
+                    onClick={()=> setType(tab)}                    
                 >
                     {tab}
                 </button>
@@ -34,10 +50,20 @@ function Content() {
                 value={title}
                 onChange={e=>setTitle(e.target.value)}
             />
-            <ul>
-                {posts.map(post => (<li key= {post.id}>{post.title || post.name}</li>))}
-
-            </ul>
+            {posts.map(post => (<li key= {post.id}>{post.title || post.name}</li>))}
+            {showGoToTop && (
+                <button
+                    style={{
+                        position: 'fixed',
+                        right: 20,
+                        bottom: 20,
+                        borderRadius: 100
+                        
+                    }}
+                
+                >Go to top</button>
+            )}
+            
         </div>
     )
 
