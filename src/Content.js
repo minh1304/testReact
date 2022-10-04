bd2ff91a28c5babe40909652fba331c7f9ed94f9
minh1304 <vtm1304@gmail.com>
2022-10-04 23:40:37 +0700

    useEffect review avatar

--- src/Content.js
+++ src/Content.js
@@ -1,71 +1,33 @@
 import {useEffect,useState} from 'react'
 const tabs = ['posts', 'comments', 'albums']
+
 function Content() {
 
-    const [title, setTitle] =useState('')
-    const [posts, setPost] = useState([])
-    const [type, setType] = useState('posts')
-    const [showGoToTop,setShowGoToTop] = useState(false)
-
-
-    useEffect(()=>{
-        fetch(`https://jsonplaceholder.typicode.com/${type}`)
-        .then(res =>res.json()) // Obj đc convert sang JS
-        .then(posts => {
-            setPost(posts)
-        })
-    }, [type])
-
-    useEffect( () => {
-        const handleScroll = () => {
-            if(window.scrollY >=200) {
-                setShowGoToTop(true)
-            }
-            else {
-                setShowGoToTop(false)
-            }
-            //setShowGoToTop(window.scrollY >=200)
-        }
-        window.addEventListener('scroll', handleScroll)
-        //Clean function
+    const [avatar, setAvatar] = useState()
+    useEffect(()=> {
+        //Cleanup
         return () => {
-            window.removeEventListener('scroll', handleScroll)
+            avatar && URL.revokeObjectURL(avatar.preview)
         }
-    }, [])
+    }, [avatar])
+    const handlePreviewAvatar =(e) => {
+        const file = e.target.files[0]
+        file.preview = URL.createObjectURL(file)  
+        setAvatar(file)
+    }
+
     return (
         <div>
-            {tabs.map(tab => (
-                <button 
-                    key={tab}
-                    style = {type === tab ? {
-                        color: '#fff',
-                        backgroundColor: '#333'
-                    } : {}}
-                    onClick={()=> setType(tab)}                    
-                >
-                    {tab}
-                </button>
-            ))}
             <input 
-                value={title}
-                onChange={e=>setTitle(e.target.value)}
+                type="file"
+                onChange={handlePreviewAvatar}
             />
-            {posts.map(post => (<li key= {post.id}>{post.title || post.name}</li>))}
-            {showGoToTop && (
-                <button
-                    style={{
-                        position: 'fixed',
-                        right: 20,
-                        bottom: 20,
-                        borderRadius: 100
-                        
-                    }}
-                
-                >Go to top</button>
+            {avatar && (
+                <img src={avatar.preview} width = "30%"></img>
             )}
-            
         </div>
     )
+    
 
 }
 export default Content
