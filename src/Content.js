cd4c4ab961aa3036c82e5fe9eeb9837d1f491c90
minh1304 <vtm1304@gmail.com>
2022-10-05 23:54:27 +0700

    fake chatApp

--- src/Content.js
+++ src/Content.js
@@ -2,31 +2,7 @@ import {useEffect,useState} from 'react'
 const tabs = ['posts', 'comments', 'albums']
 
 function Content() {
-
-    const [avatar, setAvatar] = useState()
-    useEffect(()=> {
-        //Cleanup
-        return () => {
-            avatar && URL.revokeObjectURL(avatar.preview)
-        }
-    }, [avatar])
-    const handlePreviewAvatar =(e) => {
-        const file = e.target.files[0]
-        file.preview = URL.createObjectURL(file)  
-        setAvatar(file)
-    }
-
-    return (
-        <div>
-            <input 
-                type="file"
-                onChange={handlePreviewAvatar}
-            />
-            {avatar && (
-                <img src={avatar.preview} width = "30%"></img>
-            )}
-        </div>
-    )
+    const [lessonID, setLessonID] = useState(1)
     
 
 }
