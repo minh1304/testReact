1189439ff6eefbf888e36a1510546622fac98ff5
minh1304 <vtm1304@gmail.com>
2022-10-06 00:39:18 +0700

    update

--- src/Content.js
+++ src/Content.js
@@ -1,9 +1,42 @@
-import {useEffect,useState} from 'react'
-const tabs = ['posts', 'comments', 'albums']
+import React, {useEffect,useState} from 'react'
+const lessons = [
+    {
+        id: 1,
+        name: 'React JS là gì? tại sao nên học?'
+    },
+    {
+        id: 2,
+        name: 'SPA/MPA là gì?'
+    },
+    {
+        id: 3,
+        name: 'Arrow function'
+    }
+]
 
 function Content() {
     const [lessonID, setLessonID] = useState(1)
-    
+    return (
+        <div>
+            <ul>
+                {lessons.map(lesson => (
+                    <li
+                        key={lesson.id}
+                        style={{
+                            color: lessonID === lesson.id?
+                                'red' : 
+                                '#333',
+                            cursor: 'pointer'
+
+                        }}
+                        onClick= {()=>setLessonID(lesson.id)}
+                    >
+                        {lesson.name}
+                    </li>
+                ))}
+            </ul>
+        </div>
+    )
 
 }
 export default Content
